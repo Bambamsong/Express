e95761a38f8e27dc6f57402bbe066824c0ec9691
Jinyong <jinyong@Jinyongui-noteubug.local>
2024-06-10 22:59:37 +0900

    CORS 사용

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cookieParser = require("cookie-parser"); // 사용자 인증 미들웨어
 const app = express();
 const port = 9000;
-
+// CORS
+const cors = require("cors");
+// env 파일
 require('dotenv').config(); 
 // 스웨거
 const swaggerJsdoc = require('swagger-jsdoc');
@@ -18,21 +20,28 @@ connect();
 // const commentRouter = require("./routes/comments"); // 댓글
 const indexRouter = require("./routes/index");
 
+// 모든 요청에 대해 CORS 활성화
+app.use(cors());
+// CORS에 추가 옵션 설정 (선택 사항)
+const corsOptions = {
+    origin: '*', // 모든 도메인 허용
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // 자격 증명 허용
+};
+// 모든 요청에 대해 CORS 옵션 적용
+app.use(cors(corsOptions));
+
+
 app.use(express.json()); // JSON 형식의 요청 본문을 파싱(parsing)하기 위한 미들웨어를 설정
 app.use(express.urlencoded({ extended: false })); // URL-encoded 형식의 요청 본문을 파싱하기 위한 미들웨어를 설정
 app.use(cookieParser()); // 사용자인증
-// app.use("/api", [userRouter, authRouter, postRouter, commentRouter]);
 app.use("/api", [indexRouter]);
 
-// app.get('/', (req, res) => {
-//     res.send("Hello Jinyong!");
-// })
 
 app.listen(port, () => {
     console.log(port, "Server is running!");
 });
-
-
 
 // Swagger 설정
 const swaggerDefinition = {
